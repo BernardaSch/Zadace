22bd803eeeeb600797198bb9df5ff7d763619919
Bernarda <bernarda.schwalba@gmail.com>
2024-10-13 19:57:05 +0200

    Drugi zadatak

--- Main.java
+++ Main.java
@@ -1,26 +1,22 @@
-/*Zadatak 1: Brojevi od 1 do N
-Napišite program koji će izračunati sumu svih brojeva
- od 1 do N, gdje je N pozitivan cijeli broj unesen od strane korisnika.*/
-
+/*Zadatak 2: Brojanje znamenki broja
+Napiši program koji prima cijeli broj od
+korisnika i  izračuna broj znamenki unesenog broja.*/
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int broj = 0;
-        int zbroj = 0;
+        int broj = 1;
+        int brojac = 0;
 
-        do{
-            System.out.println("Unesite cijeli pozitivni broj: ");
-            broj = sc.nextInt();
-        }while (broj<0);
+        System.out.println("Unesite broj: ");
+        broj = sc.nextInt();
 
-        for(int i = 0; i<= broj; i++){
-            zbroj = zbroj + i;
+        while(broj > 0){
+            broj = broj/10;
+            brojac = brojac+1;
         }
-        System.out.println("Zbroj unesenih brojeva je: " + zbroj);
+        System.out.println("Broj koji ste unijeli sadrži " + brojac + " znamenki.");
     }
 }
-
